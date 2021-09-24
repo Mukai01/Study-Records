436388d7b2393a673cb8a5db1a13b902f6415e73
Nakamukai1 <nakamukaiya@gmail.com>
2021-09-24 23:18:51 +0900

    9/24 勉強内容を反映

--- SQL/6. 副問い合わせ.sql
+++ SQL/6. 副問い合わせ.sql
@@ -47,3 +47,25 @@ SELECT * FROM 家計簿アーカイブ
 
 SELECT * FROM 家計簿アーカイブ
  WHERE 費目 IN (SELECT COALESCE(費目, '不明') FROM 家計簿)
+
+
+-- 4. 表の結果となる副問い合わせ
+-- 家計簿と家計簿アーカイブの一部をつなげて、SUMで合計額を計算
+-- テーブルにはSUBという名前を付けている
+SELECT SUM(SUB.出金額) AS 出金額合計
+  FROM (SELECT 日付, 費目, 出金額
+          FROM 家計簿
+         UNION
+        SELECT 日付, 費目, 出金額
+          FROM 家計簿アーカイブ
+         WHERE 日付 >= '2018-01-01'
+           AND 日付 <= '2018-01-31') AS SUB
+
+-- 家計簿集計の4項目に、テーブルを追加する
+INSERT INTO 家計簿集計 (費目, 合計, 平均, 回数)
+-- 以下が副問い合わせ
+-- この用法はInsert文の特殊用法なので、カッコはいらない
+SELECT 費目, SUM(出金額), AVG(出金額), 0
+  FROM 家計簿
+ WHERE 出金額 > 0
+ GROUP BY 費目
