675e2be12ff8bbbcdf0ee7f257e487ee884052fc
Nakamukai1 <nakamukaiya@gmail.com>
2021-10-03 23:13:13 +0900

    10/3 勉強内容を反映

--- Django/blogproject/blogpost/urls.py
+++ Django/blogproject/blogpost/urls.py
@@ -1,14 +1,14 @@
 # 以下のコードを追加
 from django.urls import path
-from .views import BlogList
+from .views import BlogList, BlogDetail
 
 # requestにurlとあると、BlogListとして定義されたviewを呼び出す
+# BlogDetailも追加する
+# <int:pk>と書く
 urlpatterns = [
-    path('list/', BlogList.as_view()),
+    path('list/', BlogList.as_view(), name='list'),
+    path('detail/<int:pk>/', BlogDetail.as_view()),
 ]
-
-
-
 
 # 初期設定の際に書いていたもの
 # from django.contrib import admin
--- Django/blogproject/blogpost/views.py
+++ Django/blogproject/blogpost/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 # 以下のコードを追加
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import BlogModel
 
 # BlogListがListViewを継承
 class BlogList(ListView):
     template_name = 'list.html'
+    model = BlogModel
+
+# BlogDetailがDetailViewを継承
+class BlogDetail(DetailView):
+    template_name = 'detail.html'
     model = BlogModel
