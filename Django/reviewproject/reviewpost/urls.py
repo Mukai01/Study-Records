81547827fbcd2e4418f0c1e05d0df619e6675283
Nakamukai1 <nakamukaiya@gmail.com>
2021-10-12 23:58:42 +0900

    10/12 勉強内容を反映

--- Django/reviewproject/reviewpost/urls.py
+++ Django/reviewproject/reviewpost/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from .views import signupview
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('signup/', signupview, name='signup'),
 ]
--- Django/reviewproject/reviewpost/views.py
+++ Django/reviewproject/reviewpost/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# renderの第1引数はrequest, 第2引数はtemplateとして使用する
+# 第3引数はhtmlの中で、{{ somedata }} とすると使うことが可能
+def signupview(request):
+    # print文を入れるとterminalで表示される
+    # print('signup function is called')
+    
+    # username_dataを表示させる
+    print(request.POST.get('username_data'))
+    return render(request, 'signup.html', {'somedata':100})
