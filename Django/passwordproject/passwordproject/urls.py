07715ed2caeba61b43bae6842a23b28817101166
Nakamukai1 <nakamukaiya@gmail.com>
2021-10-26 00:12:53 +0900

    10/25 勉強内容を反映

--- Django/passwordproject/passwordproject/urls.py
+++ Django/passwordproject/passwordproject/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 # authアプリケーションを呼び出す
+# change-passwordが入っている場合、auth_viewsで定義されているPasswordChangeViewクラスを呼び出す
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('accounts/', include('django.contrib.auth.urls')),
+    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='change-password.html'),),
 ]
